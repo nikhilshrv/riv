c7a978de8177a69a46b6ce7cf8ac1d4cf6051b17
Nikhil Shrivastav <nikhil.shrivastav@live.com>
2019-01-23 22:35:05 +0530

    added ticker

---
+++ frontend/src/app/index/index.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { IndexRoutingModule } from './index-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    IndexRoutingModule
+  ]
+})
+export class IndexModule { }
